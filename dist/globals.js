e1cd71befe59fcb975e86104314d3f5e63d22363
ravengerUA <qe1dg7bm>
2015-08-26 19:15:26 +0300

    remove dist

--- dist/globals.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Globals = {};
-Globals.imageCdnUrl = 'https://data.fantlab.ru/images/editions/small/';
-Globals.apiUrl = 'http://aspnet5-bookportal-web.azurewebsites.net/api';
-
-module.exports = Globals;
-//# sourceMappingURL=globals.js.map
